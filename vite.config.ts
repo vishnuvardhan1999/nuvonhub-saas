93be6da0d86057a5a3dc0c70fe89f0d5a39a9824
Kesavarapu Vishnuvardhan Reddy <vishnuvardhanmec@gmail.com>
2025-10-28 14:26:40 +0000

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Always use ESM syntax here.
 export default defineConfig({
+  plugins: [react()],
   server: { host: true, port: 5173 },
   preview: { host: true, port: 3001 }
 })
-
